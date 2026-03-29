5c32ab564e3c9401cdd24127201ccfe07d3d6522
Matej Penciak <matej.penciak@gmail.com>
2026-03-29 11:49:41 -0500

    add request_id tracing layer

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,8 +5,21 @@ use axum::{
     extract::DefaultBodyLimit,
     routing::{get, post},
 };
-use tower_http::trace::TraceLayer;
+use tower_http::{
+    request_id::{MakeRequestId, RequestId, SetRequestIdLayer},
+    trace::TraceLayer,
+};
 use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
+
+#[derive(Clone)]
+struct Id;
+
+impl MakeRequestId for Id {
+    fn make_request_id<B>(&mut self, _request: &http::Request<B>) -> Option<RequestId> {
+        let id = uuid::Uuid::now_v7().to_string();
+        Some(RequestId::new(id.parse().unwrap()))
+    }
+}
 
 use benchwarmer_server::{routes, storage};
 
@@ -42,7 +55,21 @@ async fn main() {
             get(routes::get_report_pr),
         )
         .layer(DefaultBodyLimit::max(100 * 1024 * 1024)) // 100MB
-        .layer(TraceLayer::new_for_http())
+        .layer(
+            TraceLayer::new_for_http().make_span_with(|request: &http::Request<_>| {
+                let request_id = request
+                    .headers()
+                    .get("x-request-id")
+                    .and_then(|v| v.to_str().ok())
+                    .unwrap_or("unknown");
+
+                tracing::info_span!(
+                    "request",
+                    request_id = %request_id,
+                )
+            }),
+        )
+        .layer(SetRequestIdLayer::x_request_id(Id))
         .with_state(storage);
 
     tracing::info!(%addr, "Listening");
